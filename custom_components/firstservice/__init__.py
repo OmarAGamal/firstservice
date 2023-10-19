0e7e3638c6567493d6d42a8639b8fe0512673989
Omar Abdelmoneim Gamal <58474037+OmarAGamal@users.noreply.github.com>
2023-10-19 15:16:36 +0300

    __init__.py

--- custom_components/firstservice/__init__.py
+++ custom_components/firstservice/__init__.py
@@ -13,23 +13,26 @@ def setup(hass, config):
         from influxdb import InfluxDBClient
         from datetime import datetime
 
-        client = InfluxDBClient('192.168.100.135', 8086, 'skarpt', 'skarpt', 'skarpt')
+        client = InfluxDBClient('192.168.100.144', 8086, 'skarpt', 'skarpt', 'skarpt')
         client.create_database('test')
         client.get_list_database()
         json_payload = []
-        # data = {
-        #     "measurement": "Tzone",
-        #     "tags": {
-        #         "sensor": "824682349"
-        #     },
-        #     "time": datetime.now(),
-        #     "fields": {
-        #         'value': 252
-        #     }
-        # }
+        data = {
+            "measurement": "Tzone",
+            "tags": {
+                "sensor": "824682349"
+            },
+            "time": datetime.now(),
+            "fields": {
+                'value': 252
+            }
+        }
+        json_payload.append(data)
+        client.write_points(json_payload)
+        print("json_payload of zwave data= ",json_payload)
         json_payload.append(event.data)
         client.write_points(json_payload)
-        print("json_payload of zwave data=",json_payload)
+        print("json_payload of zwave data= ",json_payload)
         # json_payload.append(event.data)
         # client.write_points(json_payload)
         result = client.query('select * from Tzone;')
